c4b83a31aeb058de0f8db1d8b656d155434e8ed6
Heber <hebergonza@gmail.com>
2020-09-02 22:20:12 -0400

    finished 00 thru 04

--- src/03_modules.py
+++ src/03_modules.py
@@ -6,26 +6,38 @@ level operating system functionality.
 """
 
 import sys
+import os
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print("\ncommand line arguments in sys.argv, one per line")
+for i in sys.argv:
+    print(i)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print("\nOS platform")
+print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print("\nversion of Python")
+print(sys.version)
 
-
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print("\ncurrent process ID")
+print(os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print("\ncurrent working directory")
+print(os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print("\nmachine's login name")
+print(os.getlogin())
