9ace4005f056ad90edfa1b4fa336e3a3c3807514
Heber <hebergonza@gmail.com>
2020-09-05 17:02:54 -0400

    finished 11 thru 12

--- src/11_args.py
+++ src/11_args.py
@@ -17,6 +17,14 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+
+def f2(*args):
+    if isinstance(args[0], list):
+        return sum(args[0])
+    else:
+        return sum(args)
+
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -34,6 +42,11 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+
+def f3(num1, num2=1):
+    return num1 + num2
+
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -47,6 +60,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f"key: {key}, value: {value}")
+
 
 # Should print
 # key: a, value: 12
@@ -65,4 +82,5 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
+
